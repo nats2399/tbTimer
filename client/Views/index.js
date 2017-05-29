8394034c67e84640c6354de4c44f0e70776c34fd
nats2399 <nathalieaysa@gmail.com>
2017-05-29 13:19:24 -0500

    The first commit

---
+++ client/Views/index.js
@@ -0,0 +1,51 @@
+/**
+ * Created by nathalieaysa on 5/29/17.
+ */
+
+if (Meteor.isClient) {
+    Template.register.events({
+        'submit form': function(event) {
+            event.preventDefault();
+            console.log("Form submitted.");
+        }
+    });
+}
+
+Template.register.events({
+    'submit form': function(event) {
+        event.preventDefault();
+        var emailVar = event.target.registerEmail.value;
+        var passwordVar = event.target.registerPassword.value;
+        Accounts.createUser({
+            email: emailVar,
+            password: passwordVar
+        });
+    }
+});
+
+Template.login.events({
+    'submit form': function(event){
+        event.preventDefault();
+        var emailVar = event.target.loginEmail.value;
+        var passwordVar = event.target.loginPassword.value;
+        Meteor.loginWithPassword(emailVar, passwordVar);
+    }
+});
+
+Template.dashboard.events({
+    'click .logout': function(event){
+        event.preventDefault();
+        Meteor.logout();
+    }
+});
+
+
+
+/*
+Accounts.createUser()
+Accounts.changePassword()
+Accounts.forgotPassword()
+Accounts.resetPassword()
+Accounts.setPassword()
+Accounts.verifyEmail()
+*/
